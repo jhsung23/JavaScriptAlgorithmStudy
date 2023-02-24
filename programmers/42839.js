18a5c4c84dcfeb974421a7dccf808156bca10ae8
sungjihyun <jhsung23@gmail.com>
2023-02-24 14:41:50 +0900

    refactor(42839): 소수 찾기

--- programmers/42839.js
+++ programmers/42839.js
@@ -1,40 +1,36 @@
 function solution(numbers) {
+  const numArr = numbers.split('');
   const set = new Set();
 
-  for (let i = 1; i <= numbers.length; i++) {
-    permutation([...numbers], i).forEach((value) => {
-      set.add(Number(value.join('')));
+  for (let i = 1; i <= numArr.length; i++) {
+    [...getPermutation(numArr, i)].forEach((e) => {
+      set.add(Number(e.join('')));
     });
   }
 
-  return Array.from(set).filter(isPrimeNumber).length;
+  return Array.from(set).filter(isPrime).length;
 }
-
-function permutation(arr, selectNum) {
-  if (selectNum === 1) return arr.map((v) => [v]);
+function getPermutation(arr, selectNum) {
+  if (selectNum === 1) return arr.map((e) => [e]);
 
   const result = [];
 
-  arr.forEach((v, idx, arr) => {
-    const fixer = v;
-    const restArr = arr.filter((_, index) => index !== idx);
-    const permuationArr = permutation(restArr, selectNum - 1);
-    const combineFixer = permuationArr.map((v) => [fixer, ...v]);
-    result.push(...combineFixer);
+  arr.forEach((num, idx, arr) => {
+    const fix = num;
+    const rest = arr.filter((e, i) => i !== idx);
+    const permutationOfRest = getPermutation(rest, selectNum - 1);
+    const combine = permutationOfRest.map((e) => [fix, ...e]);
+    result.push(...combine);
   });
 
   return result;
 }
+function isPrime(num) {
+  if (num <= 1) return false;
+  if (num === 2) return true;
 
-function isPrimeNumber(value) {
-  if (value < 2) {
-    return false;
-  }
-
-  for (let i = 2; i <= Math.sqrt(value); i++) {
-    if (value % i === 0) {
-      return false;
-    }
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) return false;
   }
 
   return true;
