00758c705a37cab29810f5af121790977a9e76e4
sungjihyun <jhsung23@gmail.com>
2023-03-04 20:33:34 +0900

    feat(12945): 피보나치 수

---
+++ programmers/12945.js
@@ -0,0 +1,9 @@
+function solution(n) {
+  const fibo = [0, 1];
+
+  for (let i = 2; i <= n; i++) {
+    fibo[i] = (fibo[i - 1] % 1234567) + (fibo[i - 2] % 1234567);
+  }
+
+  return fibo[n] % 1234567;
+}
