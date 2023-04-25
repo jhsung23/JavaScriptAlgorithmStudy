22724c343f19f26533b2da9d37829aa4996b9c5d
sungjihyun <jhsung23@gmail.com>
2023-04-25 11:10:12 +0900

    feat(43165): 타겟 넘버

---
+++ programmers/43165.js
@@ -0,0 +1,16 @@
+function solution(numbers, target) {
+  let count = 0;
+
+  function dfs(result, depth) {
+    if (depth === numbers.length) {
+      if (result === target) count += 1;
+    } else {
+      dfs(result - numbers[depth], depth + 1);
+      dfs(result + numbers[depth], depth + 1);
+    }
+  }
+
+  dfs(0, 0);
+
+  return count;
+}
